e646eaee78192bd4836b7a055c9b50d730e0d6ed
kevin hauser <84hauser@gmail.com>
2016-04-25 16:28:05 -0400

    Added files via upload

---
+++ test/NievergeltsTLS/no-constraints/run_kHelix.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+grid=1080
+
+##########################
+for i in $(echo 10xyz); do
+count=10
+##########################
+#
+#
+##############################################################
+#count=2
+#for i in $(echo 3xyz 4xyz 5xyz 6xyz 7xyz 8xyz 9xyz 10xyz); do
+#count=$((count+1))
+##############################################################
+#
+#
+cat > input.$count << EOF
+inputhelix NewNievergelt.$i
+helixout_name testhelix.$count.$grid.out
+coord_type 0
+num_grid $grid
+natoms $count
+nframes 1
+grid_phi_beg 0
+grid_phi_end 180
+grid_theta_beg 0
+grid_theta_end 180
+print_to_plot 1
+print_step 0
+EOF
+#
+#
+/Users/k/Desktop/Manuscripts/kHelix/Code/kHelix.o input.$count
+#
+#
+done
