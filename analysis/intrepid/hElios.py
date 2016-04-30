e68137f871e78422a272a5462a737b30c048aec4
kevin hauser <84hauser@gmail.com>
2016-04-30 15:01:01 -0400

    Update hElios.py
    BUG: figure out how to get the helix() function to return x,y,z coordinates in one call -- (LINE 41) and where it gets called in main (LINE 78)
    see this SX thread: http://stackoverflow.com/questions/9752958/how-can-i-return-two-values-from-a-function-in-python
    
    numpy for math stuff like cosines...
    
    math for math stuff...
    
    scipy.constants for implicit definition of pi (too lazy to type it out...)
    
    fractions deals with the "1/8" == "0.125" because i like the way "1/8" looks in my input arrays:
    
    input parameters:
    numberOsamples == this is the number of times we apply noise to a set of helical parameters. i have this set to "2" now just to make sure code works right... later set this to "512" or something significant...
    
    stpturn == the number of steps (points along the helix) per helical revolution... fractional values actually make sense...
    
    Rrratio == Radius-to-rise ratio. thought about this test for a few months now (semi-proactive procrastination?) and testing the spectrum of helical parameters "feels" like doing it this way makes the cleanest sense in terms of geometry: fat low helices (0 < R/r < 1), nice well-proportion helices (R/r=1), and tall thin helices (R/r > 1).
    
    pctVari == percentage of the mean value that we will use to set the value of standard deviations for applying noise... so pctVari='1' is going to gen a std dev that's 1% of the mean value of whatever parameter we-re testing in the loop...
    
    sweep == total angle subtended by the points in the helix [0,2pi)
    
    twist == angle subtended by successive points along helix..
    
    x, z, y RATHER than x,y,z because that way the helix rises along the y. if x,y,z, then the helix would rise along the z, where phi==0 and spherical coordinates get weird.

--- analysis/intrepid/hElios.py
+++ analysis/intrepid/hElios.py
@@ -1,51 +1,91 @@
 #!/usr/bin/python
-import numpy as np; import math; from scipy.constants import pi;
+import numpy as np; import math; from scipy.constants import pi; from fractions import Fraction
 
 # set constant Radius
-R=1
+R=1  #we test many "radius by scaling this using Rrratio...
+pitch=1  #we need a Rkratio below, like Rrratio... now it-s just ONE pitch for code testing...
+
 # number of iterations sampled from Gaussian to get statistics
-numberOsamples=512
-# total number of steps in the helix; NOTE: numstep+1 = number of points (atoms)
-numstep=( '1/8', '1/4', '1/2', '1', '2', '4', '8')
+numberOsamples=2
+
 # steps per turn of the helix; NOTE: stpturn/numsteps = number of turns
-stpturn=( '1/256', '1/128', '1/64', '1/32', '1/16', '1/8', '1/4', '1/2', '1', '2', '4', '8', '16', '32', '64', '128', '256' )
+stpturn=( '1/8', '1/4' ) #, '1/2', '1', '2', '4', '8' )
+
 # ratio of the Radius to the rise
-Rrratio=( '1/256', '1/128', '1/64', '1/32', '1/16', '1/8', '1/4', '1/2', '1', '2', '4', '8', '16', '32', '64', '128', '256' )
+Rrratio=( '1/8', '1/4' ) #, '1/2', '1', '2', '4', '8' )
+
 # degree of std dev; values are percent of the mean value; a value of 12.5 would give a std dev that-s 12.5% of the mean
-pctVari=( '1','3.125','6.25','12.5','25','50' )
+pctVari=( '1', '3.125' )
+#-----------------------------------------------------------------------------------
+#  Define noise machine...
+def noise(pctV, iparm):
+  V=float(pctV)/float(100)
+  mean_of_distro = float(iparm)
+  std_dev_distro = float(V)*float(iparm) #stdev of distro is the percent of mean
 
-# Do each sample-draw from Gaussian         (512)
-for samples in range(numberOsamples):
+  # CHECK to make sure i get what i want:
+  #print 'V: %s' % V, 'mean_of_distro: ', mean_of_distro, 'std_dev_distro: ', std_dev_distro
+ 
+  # Damien --- is the abs(std_dev_distro) below legal? it-s the variance that drives Gaussian (stdev**2), so i think this is ok...
+  #  i did abs() becuz i was getting negative stdev-s, i think because the 
+  oparm=np.random.normal(mean_of_distro,np.abs(std_dev_distro),size=None) 
+  return oparm
+#-----------------------------------------------------------------------------------
+#  Define helix machine...
+def helix(axis,pitch,radius,sweep,atoms):
+  x=[];y=[];z=[]
+  twist=sweep/atoms; t=0
+  for pt in range(atoms):
+    t=t+twist 
+    x.append( radius * np.sin( np.radians(t) ) ); y.append( radius * np.cos( np.radians(t) ) ); z.append( pitch  * np.radians(t)
+  return x,z,y #return side-ways helix b/c when phi=0 in spherical coordinates, things get weird.
+#-----------------------------------------------------------------------------------
+# Do total NUMber of STEPs in the helix   (7)
+x=[]; y=[]; z=[]
+for num in range(3,8,1):
+  nu = int(num)
+  # Do STePs per TURN in the helix        (17)
+  for stp in stpturn:
+    # Do Radius-Rise Ratios               (17)
+    for rrr in Rrratio:
+      # Finally, we can start making helices
+      ######################################
+      # Convert fractions in the lists to floats...
+      if '/' in stp:
+        st = float(Fraction(stp))
+      else:
+        st = float(stp)
+      if '/' in rrr:
+        rr = float(Fraction(rrr))
+      else:
+        rr = float(rrr)
+      # Make perfectly regular helix, so we can break it precisely below
+      numturns = float(st) / float(nu)
+      numatoms = int(nu) + 1
+      totsweep = numturns * 2 * pi
+      steprise = R / rr              # Radius / ratio_(Radius/rise) = rise
+      heradius = R * rr              # Radius * ratio_(Radius/rise) = Radius
 
-  # Do total NUMber of STEPs in the helix   (7)
-  for num in numstep:
-  
-    # Do STePs per TURN in the helix        (17)
-    for stp in stpturn:
-    
-      # Do Radius-Rise Ratios               (17)
-      for rrr in Rrratio:
-  
-        # Finally, we can start making helices
-        ######################################
-        
-        # Make perfectly regular helix, so we can break it precisely below
-        numturns = float(stpturn) / float(numstep)
-        numatoms = int(numstep) + 1
-        totsweep = numturns * 2 * pi
-        steprise = R / rrr              # Radius / ratio_(Radius/rise) = rise
-        heradius = R * rrr              # Radius * ratio_(Radius/rise) = Radius
-        
-        # First, let-s do PITCH
-        for varians in pctVari:
+      # CHECKto make sure what i get is what i want
+      print 'num_atoms: %s' % numatoms, 'total_steps: ', num, 'steps_per_turn: ', stp, 'Radius-rise_ratio', rrr
+      # First, let-s do PITCH
+      for varians in pctVari:
+        # Do the samples down here...
+        out=''
+        for samples in range(numberOsamples):
           pitch = noise( varians, pitch ) 
-          axis=0; radius=heradius; sweep=totsweep; nturns=numturns
-          helix(axis, pitch, radius, sweep,  nturns )
+          axis=0; radius=heradius; sweep=totsweep; nturns=numatoms;
+          x,z,y = helix(axis, pitch, radius, sweep,  nturns )
+          out=out+str(x)+' '+str(z)+' '+str(y)+'\n'
+#turn-off file IO for now...
+"""
+        with open('output_num'+str(nu)+'.stp'+str(st)+'.Rrr'+str(rr)+'.var'+str(varians), 'w') as f:
+          f.write(out)
+"""
           
         # Second, let-s do RADIUS
-        for varians in pctVari:
+"""        for varians in pctVari:
           radius = noise( varians, radius )
-          axis=0; pitch=pitch; sweep=totsweep; nturns=numturns
+          axis=0; pitch=pitch; sweep=totsweep; nturns=numatoms; #nturns=numturns
           helix(axis, pitch, radius, sweep, nturns )
-      
-        
+"""
